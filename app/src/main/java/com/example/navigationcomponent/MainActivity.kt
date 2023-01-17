cf3c0521f1b2355c26540603e99788c53a17a3b9
VinayTShetty <vinaytshetty@gmail.com>
2023-01-17 11:09:09 +0530

    Action bar Color Change as per the Menu click is completed.

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.navigationcomponent
 
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -12,28 +15,53 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController:NavController
-    private lateinit var drawerLayout:DrawerLayout
-    private lateinit var appBarConfigaration:AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfigaration: AppBarConfiguration
 
     private lateinit var navigationView: NavigationView
-    private lateinit var listner:NavController.OnDestinationChangedListener
+    private lateinit var listner: NavController.OnDestinationChangedListener
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController=findNavController(R.id.fragment)
-        drawerLayout=findViewById(R.id.drawerLayout)
-        appBarConfigaration= AppBarConfiguration(navController.graph,drawerLayout)
+        navController = findNavController(R.id.fragment)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        appBarConfigaration = AppBarConfiguration(navController.graph, drawerLayout)
 
-        navigationView=findViewById(R.id.navigationView)
+        navigationView = findViewById(R.id.navigationView)
         navigationView.setupWithNavController(navController)
-        setupActionBarWithNavController(navController,appBarConfigaration)
+        setupActionBarWithNavController(navController, appBarConfigaration)
+
+        listner = NavController.OnDestinationChangedListener { controller, destination, arguments ->
+            if (destination.id == R.id.frist) {
+                supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.frist_fragment_action_bar)))
+            } else if (destination.id == R.id.second) {
+                supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.second_fragment_action_bar)))
+
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController=findNavController(R.id.fragment)
-        return navController.navigateUp(appBarConfigaration)|| super.onSupportNavigateUp()
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp(appBarConfigaration) || super.onSupportNavigateUp()
+    }
+
+    /**
+     * Its used for ToolBar color Change
+     */
+    override fun onPause() {
+        super.onPause()
+        navController.removeOnDestinationChangedListener(listner)
+    }
+    /**
+     * Its used for ToolBar color Change
+     */
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(listner)
     }
 }
