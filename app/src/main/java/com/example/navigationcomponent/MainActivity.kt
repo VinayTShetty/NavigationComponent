4fc320b85abe17d7feb55eac5f0f97f4f2ba98d9
VinayTShetty <vinaytshetty@gmail.com>
2023-01-17 08:36:55 +0530

    Drawer layout Icon Visibility completed.

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController:NavController
+    private lateinit var drawerLayout:DrawerLayout
+    private lateinit var appBarConfigaration:AppBarConfiguration
+
+    private lateinit var navigationView: NavigationView
+    private lateinit var listner:NavController.OnDestinationChangedListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController=findNavController(R.id.fragment)
+        drawerLayout=findViewById(R.id.drawerLayout)
+        appBarConfigaration= AppBarConfiguration(navController.graph,drawerLayout)
+
+        navigationView=findViewById(R.id.navigationView)
+        navigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfigaration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController=findNavController(R.id.fragment)
+        return super.onSupportNavigateUp()
     }
 }
