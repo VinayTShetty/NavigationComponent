d99a7b35338d99acd25cf800a75b982b2bfb5db5
VinayTShetty <vinaytshetty@gmail.com>
2023-01-17 10:54:51 +0530

    Drawer Menu icon click action added in the applicaion.

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         navController=findNavController(R.id.fragment)
         drawerLayout=findViewById(R.id.drawerLayout)
         appBarConfigaration= AppBarConfiguration(navController.graph,drawerLayout)
--- app/src/main/java/com/example/navigationcomponent/Second.kt
+++ app/src/main/java/com/example/navigationcomponent/Second.kt
@@ -11,14 +11,14 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 
 class Second : Fragment() {
-    val secondArgs:SecondArgs by navArgs()
+   // val secondArgs:SecondArgs by navArgs()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view= inflater.inflate(R.layout.fragment_second, container, false)
-        val numebr=secondArgs.number
+     //   val numebr=secondArgs.number
         view.findViewById<Button>(R.id.btn_fragmenttwo).setOnClickListener(){
             Navigation.findNavController(view).navigate(R.id.action_second_to_frist)
         }
-        view.findViewById<TextView>(R.id.txtvw_fragment_two).text=numebr.toString()
+       // view.findViewById<TextView>(R.id.txtvw_fragment_two).text=numebr.toString()
         return view
     }
 }
